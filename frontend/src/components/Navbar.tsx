99ce314d108c3f0a1bfb41c1bf82c3ae41b6ddde
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2025-03-15 04:02:25 +0100

    fix(frontend): increase z-index of Navbar for improved visibility

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -8,7 +8,7 @@ interface NavbarProps {
 
 const Navbar = ({setIsNavSidebarOpen}: NavbarProps) => {
     return (
-        <nav className="fixed top-0 h-[60px] md:h-[70px] left-0 bg-white w-full z-10">
+        <nav className="fixed top-0 h-[60px] md:h-[70px] left-0 bg-white w-full z-50">
             <div className="max-w-container w-full my-0 mx-auto px-2 md:px-4 flex gap-2 items-center h-full">
                 <Menu className="cursor-pointer" onClick={() => setIsNavSidebarOpen(true)} />
                 <Link href={"/"} className="text-xl md:text-3xl font-semibold">
