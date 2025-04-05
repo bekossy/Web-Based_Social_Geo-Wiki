5c0b90f11a01fa259555e23ef06fe26d3fe979df
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2025-04-05 12:59:23 +0200

    fix: update error handler to use StatusCodes for consistent error responses

--- backend/src/middleware/error-handler.ts
+++ backend/src/middleware/error-handler.ts
@@ -11,17 +11,17 @@ const errorHandlerMiddleware = (err: any, req: Request, res: Response, next: Nex
         customError.msg = Object.values(err.errors)
             .map((item: any) => item.message)
             .join(", ")
-        customError.statusCode = 400
+        customError.statusCode = StatusCodes.BAD_REQUEST
     }
     if (err.code && err.code === 11000) {
         customError.msg = `Duplicate value entered for ${Object.keys(
             err.keyValue
         )} field, please choose another value`
-        customError.statusCode = 400
+        customError.statusCode = StatusCodes.BAD_REQUEST
     }
     if (err.name === "CastError") {
         customError.msg = `No item found with id: ${err.value}`
-        customError.statusCode = 404
+        customError.statusCode = StatusCodes.NOT_FOUND
     }
 
     res.status(customError.statusCode).json({msg: customError.msg})
