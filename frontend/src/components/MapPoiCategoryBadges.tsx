5defff58ba32e9ac36c2d235d0332e767ccb8464
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2025-04-06 23:31:10 +0200

    feat: extend MapPoiCategoryBadges to accept additional props for Badge customization

--- frontend/src/components/MapPoiCategoryBadges.tsx
+++ frontend/src/components/MapPoiCategoryBadges.tsx
@@ -2,11 +2,14 @@ import React from "react"
 import {Badge} from "./ui/badge"
 import {type SearchBoxFeatureProperties} from "@mapbox/search-js-core/dist/searchbox/types"
 
-const MapPoiCategoryBadges = ({featureProps}: {featureProps: SearchBoxFeatureProperties}) => {
+const MapPoiCategoryBadges = ({
+    featureProps,
+    ...props
+}: {featureProps: SearchBoxFeatureProperties} & React.ComponentProps<typeof Badge>) => {
     return (
         <div className="flex items-center gap-1.5 flex-wrap">
             {featureProps?.poi_category?.map((feature: string, index: number) => (
-                <Badge variant="outline" key={index} className="bg-primary/5 text-xs">
+                <Badge variant="outline" key={index} className="bg-primary/5 text-xs" {...props}>
                     {feature}
                 </Badge>
             ))}
