ec30511a478e756901f5c88ba5d6dd790fb2201e
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2025-05-17 16:56:36 +0200

    feat: remove unused Twitter and OpenGraph images and their alt text files

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -26,6 +26,36 @@ export const metadata: Metadata = {
     authors: [{name: "Kaosisochukwu Bruno Ezealigo", url: "https://kaosiso-ezealigo.netlify.app/"}],
     creator: "Kaosisochukwu Bruno Ezealigo",
     robots: "index, follow",
+    openGraph: {
+        title: "Social Geo-Wiki | Collaborative Mapping Platform",
+        description:
+            "A web-based mapping platform that allows users to collaboratively annotate locations with comments, images, and tags. Built with Next.js, Express, MongoDB, and Mapbox.",
+        url: "https://web-based-social-geo-wiki.vercel.app/",
+        siteName: "Social Geo-Wiki",
+        images: [
+            {
+                url: "/social_geo-wiki.png",
+                width: 1200,
+                height: 630,
+                alt: "Social Geo-Wiki",
+            },
+        ],
+        type: "website",
+    },
+    twitter: {
+        title: "Social Geo-Wiki | Collaborative Mapping Platform",
+        description:
+            "A web-based mapping platform that allows users to collaboratively annotate locations with comments, images, and tags. Built with Next.js, Express, MongoDB, and Mapbox.",
+        card: "summary_large_image",
+        images: [
+            {
+                url: "/social_geo-wiki.png",
+                width: 1200,
+                height: 630,
+                alt: "Social Geo-Wiki",
+            },
+        ],
+    },
 }
 
 export default function RootLayout({
